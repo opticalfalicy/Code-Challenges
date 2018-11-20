f467b5013a32025ee6e865053fbd945e87e63c55
Unknown <opticalfalicy@gmail.com>
2018-11-20 12:56:43 -0600

    BinaryAddition complete

---
+++ Javascript/November 2018/BinaryAddition.js
@@ -0,0 +1,8 @@
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+function addBinary(a,b) {
+    // Using 2 as the radix to produce a binary conversion/
+    return (a+b).toString(2);
+  }
