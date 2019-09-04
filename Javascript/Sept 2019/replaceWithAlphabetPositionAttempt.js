fe65df49d4f50260dba0a331639c8c99e5e64dc6
Unknown <opticalfalicy@gmail.com>
2019-09-04 00:27:42 -0400

    replaceWithAlphabetPositionAttempt.js

---
+++ Javascript/Sept 2019/replaceWithAlphabetPositionAttempt.js
@@ -0,0 +1,84 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+function alphabetPosition (text) {
+  // console.log(text)
+  //   let letters = new RegExp('^[A-Za-z]+$');
+  //   console.log(letters);
+  //   let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  //   if(text.match(letters)){
+  //     return alphabet.indexOf(text)+1;
+  //   }
+
+  let abet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  for (let i = 0; i <= abet.length; i++) {
+    if (abet[i] === text) {
+      console.log (text);
+      return abet.parseInt (text);
+    }
+  }
+}
+
+//   Test.assertEquals(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+// Test.assertEquals(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
