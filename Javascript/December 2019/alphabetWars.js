174f64a9131ff9fe490b58cc4f4de51afb98eeb3
opticalfalicy <opticalfalicy@gmail.com>
2019-12-13 11:39:49 -0500

    attempt 2.0

--- Javascript/December 2019/alphabetWars.js
+++ Javascript/December 2019/alphabetWars.js
@@ -1,28 +1,80 @@
 function alphabetWar(battlefield) {
 
-    let letArr = battlefield.split('');
-    let bCnt = 0;
-    let sZStart, sZEnd;
-    
-    for(let i = 0; i < letArr.length; i++){
-      if(letArr[i] == '#'){
-        ++bCnt;
-      }
-      if(bCnt == 2){
-        if(letArr[i+1] !== '['){
-        letArr.splice(0, i)
-        bCnt--;
-        }
-        if([letArr[i+1] == '['){
-          letArr[i+1] = sZStart;
-          while(letArr[i] != ']') continue;
-          if(letArr[i] == ']'){
-            
-          }
-        }
-      };
-      
-    }
+  let letArr = battlefield.split('');
+  let bCnt = 0;
+  let bFlag = false;
+  let sArr = [];
+  let sZStart;
+  let sZEnd = '';
+  let bZStart;
+  let bZEnd;
+  let seperate = false;
+  
+  for(let i = 0; i <= letArr.length; i++){
 
-    return "oops" ;
+  
+    if(letArr[i] == '#'){
+//         ++bCnt;
+      bFlag = true;
+      sArr.push(letArr[i]);
+    }
+    if(letArr[i] == '['){
+      sZStart = letArr[i];
+      sArr.push(sZStart);
+      
+      while(sZEnd === ''){
+        i++
+        if(letArr[i] == ']'){
+        sArr.push(letArr[i]);
+        sZEnd = letArr[i];
+        }
+        else sArr.push(letArr[i]);
+      }
+      sZStart = '';
+      sZEnd = '';
+      
+    } 
+  }
+  
+    if(i = letArr.length && bFlag == false){
+      let nField = battlefield.replace(/[^a-z]/g, "");
+      return nField;
+    }
+    
+    if(bFlag = true){
+    
+      for(let j = 0; j <= sArr.length; j++){
+        if(sArr[j] == '#'){
+          ++bCnt;
+          bZStart = sArr[j]
+        }
+        
+        if(sArr[j] == '[') seperate = true;
+        
+        if(bCnt == 2){
+          if(seperate == true){ 
+            sArr.splice(0, sArr[j])
+            --bCnt;
+            seperate = false;
+          }
+          
+          else{
+            while(bZEnd == undefined){
+              j++;
+              if(sArr[j] == ']' || j == sArr.length){
+                bZEnd = sArr[j];
+              }
+            }
+            
+        if(bZEnd == ']'){
+              sArr.splice(0, bZEnd);
+          }
+          
+        }
+      }
+    }
+  }
+  console.log(sArr)
+
+  return "oops" ;
 }
