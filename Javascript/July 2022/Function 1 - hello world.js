988470faf3a17d0641a341e7e4df610fa2f287fb
b <swedishgoodbye@gmail.com>
2022-07-25 13:16:45 -0500

    added codewars link

--- Javascript/July 2022/Function 1 - hello world.js
+++ Javascript/July 2022/Function 1 - hello world.js
@@ -1,5 +1,7 @@
 // Make a simple function called greet that returns the most-famous "hello world!".
 
+// https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+
 
 function greet(){
     return "hello world";
