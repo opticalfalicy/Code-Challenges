2da33460f0a30620a6334bce260078cf68866dae
b <swedishgoodbye@gmail.com>
2022-09-18 14:46:45 -0500

    reverseString.js

---
+++ Javascript/September 2022/reverseString.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+function solution(str){
+  
+    let sStr = str.split("")
+    
+    return sStr.reverse().join("");
+    
+  }
